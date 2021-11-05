ec0fb890930150081d4203be6e1286fea6f0483a
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-11-05 09:53:43 +0100

    added pace

--- js/main.js
+++ js/main.js
@@ -13,28 +13,46 @@
 //         item
 //     }
 // ))
+var typesOfPawn = {
+    whiteNormalPawn: 'wP',
+    blackNormalPawn: 'bP',
+    whiteKing: 'wK',
+    blackKing: 'bK',
+    whiteQueen: 'wQ',
+    blackQueen: 'bQ'
+}
 
-// console.log(example);
 var config = {
     position: {
-        a3: 'wP',
-        b3: 'wP',
-        c3: 'wP',
-        d3: 'wP',
-        e3: 'wP',
-        f3: 'wP',
-        g3: 'wP',
-        h3: 'wP',
-        a6: 'bP',
-        b6: 'bP',
-        c6: 'bP',
-        d6: 'bP',
-        e6: 'bP',
-        f6: 'bP',
-        g6: 'bP',
-        h6: 'bP',
+        a3: typesOfPawn.whiteNormalPawn,
+        b3: typesOfPawn.whiteNormalPawn,
+        c3: typesOfPawn.whiteNormalPawn,
+        d1: typesOfPawn.whiteQueen,
+        d3: typesOfPawn.whiteNormalPawn,
+        e1: typesOfPawn.whiteKing,
+        e3: typesOfPawn.whiteNormalPawn,
+        f3: typesOfPawn.whiteNormalPawn,
+        g3: typesOfPawn.whiteNormalPawn,
+        h3: typesOfPawn.whiteNormalPawn,
+        a6: typesOfPawn.blackNormalPawn,
+        b6: typesOfPawn.blackNormalPawn,
+        c6: typesOfPawn.blackNormalPawn,
+        d6: typesOfPawn.blackNormalPawn,
+        d8: typesOfPawn.blackQueen,
+        e8: typesOfPawn.blackKing,
+        e6: typesOfPawn.blackNormalPawn,
+        f6: typesOfPawn.blackNormalPawn,
+        g6: typesOfPawn.blackNormalPawn,
+        h6: typesOfPawn.blackNormalPawn,
     },
-    draggable: true
+    draggable: true,
+    dropOffBoard: 'snapback',
+    moveSpeed: 300,
+    snapbackSpeed: 100,
+    snapSpeed: 100,
+    snapSpeed: 50,
+    trashSpeed: 100
+
 }
 
 var board = Chessboard('myBoard', config);
